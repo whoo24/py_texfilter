b927ebaf08fc19ccd0bb7716e582a5f644e9ddaa
whoo24 <whoo24@gmail.com>
2012-09-12 16:40:24 +0900

    find textline

---
+++ texf.py
@@ -0,0 +1,34 @@
+import re
+import os
+import sys
+
+
+date_type = '[\d]+-[\d]+[\d]-[\d]+'
+time_type = '[\d]+:[\d]+:[\d]+'
+octet_type = '0x[\d]+'
+
+pattern = '^' + date_type + ' ' + time_type + ' DEBUG ' + octet_type + ' - [[' + time_type + '-\d+] (.+$)'
+
+def get_text_file(path):
+	f = open ( path,"r")
+	data = f.read()
+	return data
+
+def tex_filter(pattern, path):
+	m = re.findall(pattern, get_text_file(path), re.MULTILINE)
+	for _m in m:
+		print( _m )
+
+if len(sys.argv) < 2:
+	exit('Usage: texf.py FILENAME [PATTERN]')
+
+if len(sys.argv) == 3:
+	pattern = sys.argv[2]
+
+filename = sys.argv[1]
+if not os.path.exists(filename):
+	sys.exit('ERROR: file not found(%s)' % filename)
+
+
+tex_filter( pattern, filename )
+
